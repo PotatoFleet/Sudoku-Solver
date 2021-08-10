b6933bdeaa87701871447bd1602cc2f3fbc4ce6b
PotatoFleet <82525159+PotatoFleet@users.noreply.github.com>
2021-08-10 15:50:39 +0530

    Faster sudoku solver
    Duck

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,44 +1,8 @@
-# function to check if a number target in position pos is valid
-# a position is valid if the number target is not found in the same row, column or box
-def isValid(board, target, pos):
-
-	for i in range(9):
-
-		if (board[pos[0]][i] == target) or (board[i][pos[1]] == target) or (board[pos[0] // 3 * 3 + i // 3][pos[1] // 3 * 3 + i % 3] == target):
-
-			return False
-
-	return True
-
-# main function
-# start -> the ith empty spot
-# empty_cells -> a list of all the empty cell positions
-# board -> the board -_-
-def dfs(start, empty_cells, board):
-
-	if start == len(empty_cells):
-		return True #solved
-
-	i, j = empty_cells[start]
-
-    # Testing all numbers to fill in empty slot
-	for k in range(1, 10):
-
-		k = str(k)
-
-		if not isValid(board, k, (i, j)):
-			continue
-
-		board[i][j] = k
-
-		if dfs(start+1, empty_cells, board):
-			return True
-
-        # Reverts back to original if doesn't return True
-		board[i][j] = '.'
-
-# finding all empty cells and then calling dfs()
 def solve(board):
+
+	rows = [set() for _ in range(9)]
+	cols = [set() for _ in range(9)]
+	blocks = [set() for _ in range(9)]
 
 	empty_cells = []
 
@@ -50,4 +14,40 @@ def solve(board):
 
 				empty_cells.append([i, j])
 
-	dfs(0, empty_cells, board)
+			else:
+
+				rows[i].add(board[i][j])
+
+				cols[j].add(board[i][j])
+
+				blocks[i // 3 * 3 + j // 3].add(board[i][j])
+
+	def dfs(start):
+
+		if start == len(empty_cells): return True # Done
+
+		i, j = empty_cells[start]
+
+		for num in range(1, 10):
+
+			snum = str(num)
+			block = i // 3 * 3 + j // 3
+
+			if snum in rows[i] or snum in cols[j] or snum in blocks[block]:
+				continue
+
+			rows[i].add(snum)
+			cols[j].add(snum)
+			blocks[block].add(snum)
+			board[i][j] = snum
+
+			if dfs(start + 1): return True # if possible
+
+			rows[i].remove(snum)
+			cols[j].remove(snum)
+			blocks[block].remove(snum)
+			board[i][j] = '.'
+
+		return False
+
+	dfs(0)
